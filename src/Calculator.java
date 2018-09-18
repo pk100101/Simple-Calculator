19cdb22a3f24771da75782eae90545aba6b539fe
Nrutika <Nrutika@120-Forge.fios-router.home>
2018-09-17 22:25:06 -0400

    Homework 9/17

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,12 @@
 
-public class Calculator {
-
+public class Calculator 
+{
+	int add;{num1, num2;
+	int ans = num1 + num2;
+	return; int ans;
+	}
+	int subtract;{num1, num2
+	int ans2 = num1 - num2;
+	return; int ans2;
+	}
 }
--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -1,4 +1,8 @@
 
-public class CalculatorMain {
-
+public class CalculatorMain 
+{
+	Calculator calc = new Calculator ();
+	System.out.println("calc.add(7,3");
+	System.out.println("calc.subtract(7,3");
+	
 }
