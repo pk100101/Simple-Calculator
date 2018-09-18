8a54876d8ab6de618f54ada9963199dd0d417e14
pk100101 <42978490+pk100101@users.noreply.github.com>
2018-09-18 14:26:42 -0400

    Update CalculatorMain.java

--- src/CalculatorMain.java
+++ src/CalculatorMain.java
@@ -5,8 +5,10 @@
  */
 public class CalculatorMain 
 {
+	public static main void (String [] args)
+	{
 	Calculator calc = new Calculator ();
 	System.out.println("calc.add(7,3");
 	System.out.println("calc.subtract(7,3");
-	
+	}
 }
